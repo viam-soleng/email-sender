846800360caabfcf639689466b638586b80b8ec9
williamjhyland <williamjhyland@outlook.com>
2025-03-08 08:26:26 -0500

    absolute paths for cron

--- run-script.sh
+++ run-script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-source  ./venv/bin/activate  # Activate your virtual environment
-python3 main.py                # Run your Python script
+source /home/viam/email-sender/venv/bin/activate
+python3 /home/viam/email-sender/main.py
